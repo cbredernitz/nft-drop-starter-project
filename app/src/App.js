dfb95a663a6ba389c675030ce8c3b9e059cf54db
Chris <chrisrbredernitz@gmail.com>
2022-09-11 15:23:47 -0600

    add connection to wallet check

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,34 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const checkIfWalletIsConnected = async() => {
+      try {
+        const { solana } = window;
+
+        if (solana && solana.isPhantom) {
+          console.log('Phantom wallet found!!');
+        } else {
+          alert('Solana wallet is not found. Please get a Phantom wallet')
+        }
+      } catch (error) {
+        console.error(error);
+      }
+  }
+  
+  
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, [])
   return (
     <div className="App">
       <div className="container">
